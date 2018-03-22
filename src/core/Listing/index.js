fafce2b88d46edd9b02cb64d5d4f8c5d8fb56d39
TaylorBett <taylor.bett@hotmail.com>
2018-03-22 14:27:49 +1100

    Update alt text on listing agency logos

--- src/core/Listing/index.js
+++ src/core/Listing/index.js
@@ -12,7 +12,7 @@ class Listing extends React.PureComponent {
         return (
             <div className='listing' style={listingStyle} key={this.props.data.id}>
                 <div className='listing__image' style={listingImage} />
-                <img className='listing__logo' src={this.props.data.agency.logo} />
+                <img className='listing__logo' alt='Agency logo' src={this.props.data.agency.logo} />
                 <p className='listing__price'>{this.props.data.price}</p>
                 <button className="listing__primary-cta" onClick={() => this.props.ctaFn(this.props.data.id)}>{this.props.ctaText}</button>
             </div>
